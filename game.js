59fbde2997edbfdc3ea1c6eeecfe67e958db6362
nigberg <nigberg@gmail.com>
2023-04-10 12:31:50 +0300

    event listener for arrow keys added

--- game.js
+++ game.js
@@ -11,6 +11,33 @@ const snake = {
     cells: []
 };
 const apple = {
-    x:100,
+    x: 100,
     y: 100
 };
+
+const intervalId = setInterval(gameLoop, 100);
+
+function gameLoop(){
+
+}
+
+// set listener for arrow buttons
+document.addEventListener("keydown", (evt) => {
+    if(evt.keyCode === 37 && snake.dx !== 10){ //left
+        snake.dx = -10;
+        snake.dy = 0;
+    }
+    if(evt.keyCode === 38 && snake.dy !== 10){ //up
+        snake.dx = 0;
+        snake.dy = -10;
+    }
+    if(evt.keyCode === 39 && snake.dx !== -10){ //right
+        snake.dx = 10;
+        snake.dy = 0;
+    }
+    if(evt.keyCode === 40 && snake.dy !== -10){ //down
+        snake.dx = 0;
+        snake.dy = 10;
+    }
+
+});
